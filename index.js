76fa1ed98b647673f4ae62b478fbf55aba8ee176
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-11-26 16:43:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ function nestedTarget(){
   const nestedTarget = document.querySelector('#nested .target')
   return nestedTarget
 }
+
+function deepestChild(){
+  const childSearch = document.getElementById('grand-node').querySelectorAll('div')
+   
+  for (let i = 0; i < childSearch.length; i++) {
+    if (childSearch[i].innerHTML = "boo!"){
+    return childSearch[i]
+  }
+  else{
+    childSearch.push(childSearch[i])
+  }
+}
