7bb571cfd83dfaea0178ccc52aa6ca1c430dd3ef
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-11-26 16:34:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function getFirstSelector(selector){
+  var firstSelector = document.querySelector(selector)
+  return firstSelector
+}
+
+function nestedTarget(){
+  const nestedTarget = document.querySelector('#nested .target')
+  return nestedTarget
+}
